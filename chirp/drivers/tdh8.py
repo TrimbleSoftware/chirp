1cf7b5720b6052181800de008b4ea30ddd40727e
Jon Betts <jon.betts@gmail.com>
2026-09-17 16:33:51 +0100

    tdh8: Fix crashes when saving memories with duplex `off` or `split`
    As the values `off` and `split` aren't in expected offset values
    this throws an `ValueError` and ceases processing the rest of the
    entries in the memory, in effect resetting them to defaults.

--- chirp/drivers/tdh8.py
+++ chirp/drivers/tdh8.py
@@ -897,10 +897,10 @@ class TDH8(chirp_common.CloneModeRadio):
 
         # offset direction
         if self._has_offsetdir:
-            _offset_list = ['', '-', '+']  # None/OFF, Minus/Negitive, Plus/Positive
-            _mem.offsetdir = _offset_list.index(mem.duplex)
-            if mem.duplex == '':
-                _mem.offsetdir = 0
+            try:
+                _mem.offsetdir = ['', '-', '+'].index(mem.duplex)  # '' = Off
+            except ValueError:
+                _mem.offsetdir = 0  # Also Off
 
         # name
         if not _is_vfo:
